78e49fdb69a55186b24324f75719a6373f55882e
Michael-Njoroge <mikethecoder12@gmail.com>
2023-12-14 11:41:13 +0300

    complete the stripe payment functionality

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Stripe;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class StripeController extends Controller
 {
@@ -10,4 +12,25 @@ class StripeController extends Controller
     {
         return view('stripe');
     }
-}
+
+    public function stripePost(Request $request)
+    {
+        Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+
+        Stripe\Charge::create ([
+
+                "amount" => 100 * 100,
+                "currency" => "usd",
+                "source" => $request->stripeToken,
+                "description" => "This payment is for test purpose"
+
+        ]);
+
+        Session::flash('success', 'Payment successful!');
+
+        return back();
+
+    }
+
+    }
+
